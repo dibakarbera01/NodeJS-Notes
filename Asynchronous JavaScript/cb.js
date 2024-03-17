68ed15f3a80097c34c5e77c15a09f4657a9f5905
Dibakar <dibakarbera01@gmail.com>
2024-03-17 12:44:05 +0530

    asynchronous js

---
+++ Asynchronous JavaScript/cb.js
@@ -0,0 +1,31 @@
+const fs=require('fs')
+
+console.log("First Line")
+
+
+// let data = fs.readFileSync('f1.txt')
+// console.log("File one data -> "+data)
+
+// let data2=fs.readFileSync('f2.txt')
+// console.log("File Two Data -> "+data2)
+
+
+
+fs.readFile('f1.txt',cb1)
+// asynchronous method
+function cb1(err,data){
+    if(err){
+        console.log(err)
+    }
+    console.log("File 1 data->"+data)
+}
+
+fs.readFile('f2.txt',cb2)
+function cb2(err,data){
+    if(err){
+        console.log(err)
+    }
+    console.log("File 1 data->"+data)
+}
+
+console.log('Last Line')
